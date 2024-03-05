1e41e03bd7edde05ed2b266eceb59e7f8532574d
Jose Servin <Servinjose96@gmail.com>
2024-03-04 19:57:27 -0600

    Notes: add dbt variables and setting default values

--- dbtlearnV2/macros/variables.sql
+++ dbtlearnV2/macros/variables.sql
@@ -1,6 +1,5 @@
 {% macro learn_variables() %}
 
-    {% set your_name_jinja = "Servin" %}
-    {{ log("Hello " ~ your_name_jinja, info=True) }}
+    {{ log("Hello dbt user " ~ var("user_name", "NO USERNAME IS SET!!") ~ "!", info=True) }}
 
 {% endmacro %}
